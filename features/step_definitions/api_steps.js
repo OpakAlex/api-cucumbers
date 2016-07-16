088f00d31a5bc56b2b097055fbef622986049500
Opak Alex <opak.alexandr@gmail.com>
2016-07-16 17:37:41 +0300

    use lastResponse not lastRequest

--- features/step_definitions/api_steps.js
+++ features/step_definitions/api_steps.js
@@ -3,27 +3,27 @@
 
 module.exports = function(){
   this.When(/^I send a GET request to "([^"]*)"$/, function (url, done) {
-    this.lastResponse = this.fetch(url, {
+    this.fetch(url, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       }
-    });
-    done();
-  });
-  this.Then(/^the response status should be "([^"]*)"$/, function (status, done) {
-    this.lastResponse.then((response) => {
-      this.expect(`${response.status}`).to.equal(status);
+    }).then(response => {
+      this.lastResponse = response;
       done();
     })
   });
+
+  this.Then(/^the response status should be "([^"]*)"$/, function (status, done) {
+    this.expect(this.lastResponse.status).to.equal(Number(status));
+    done();
+  });
+
   this.Then(/^the JSON schema should be:$/, function (schema, done) {
-    this.lastResponse.then((response) => {
-      response.json().then((json) => {
-        this.expect(json).to.be.jsonSchema(JSON.parse(schema));
-        done();
-      });
+    this.lastResponse.json().then((json) => {
+      this.expect(json).to.be.jsonSchema(JSON.parse(schema));
+      done();
     });
   });
 };
